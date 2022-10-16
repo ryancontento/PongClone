1cce049813ca86a0c84bb4ec8baa661c09da3d20
ryancontento <rkc61595@gmail.com>
2022-10-16 11:09:30 -0400

    player one and two created with controls set

--- Program.cs
+++ Program.cs
@@ -1,24 +1,81 @@
-﻿using Raylib_cs;
+﻿using System.Numerics;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+using static Raylib_cs.KeyboardKey;
+using static Raylib_cs.Color;
 
 namespace Pong
 {
-    static class Program
+    public class PongClone
     {
-        public static void Main()
+        public static int Main()
         {
-            Raylib.InitWindow(800, 480, "Hello World");
+            // Initialization
+            //--------------------------------------------------------------------------------------
+            const int screenWidth = 1200;
+            const int screenHeight = 900;
 
-            while (!Raylib.WindowShouldClose())
+            InitWindow(screenWidth, screenHeight, "pong");
+
+             Vector2 playerOnePosition = new Vector2(150 - 120, 300);
+             Vector2 playerTwoPosition = new Vector2(1100, (float)screenHeight / 2);
+
+
+            SetTargetFPS(240);
+            //--------------------------------------------------------------------------------------
+
+            // Main game loop
+            while (!WindowShouldClose())    // Detect window close button or ESC key
             {
-                Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.WHITE);
+                // Player Two - arrow key controls 
+                //----------------------------------------------------------------------------------
+                // if (IsKeyDown(KEY_D))
+                //     playerOnePosition.X += 2.0f;
+                // if (IsKeyDown(KEY_A))
+                //     playerOnePosition.X -= 2.0f;
+                if (IsKeyDown(KEY_W))
+                    playerOnePosition.Y -= 2.0f;
+                if (IsKeyDown(KEY_S))
+                    playerOnePosition.Y += 2.0f;
+                //----------------------------------------------------------------------------------
 
-                Raylib.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
+                // Player Two - arrow key controls 
+                //----------------------------------------------------------------------------------
+                // if (IsKeyDown(KEY_RIGHT))
+                //     playerTwoPosition.X += 2.0f;
+                // if (IsKeyDown(KEY_LEFT))
+                //     playerTwoPosition.X -= 2.0f;
+                if (IsKeyDown(KEY_UP))
+                    playerTwoPosition.Y -= 2.0f;
+                if (IsKeyDown(KEY_DOWN))
+                    playerTwoPosition.Y += 2.0f;
+                //----------------------------------------------------------------------------------
 
-                Raylib.EndDrawing();
+                // Draw
+                //----------------------------------------------------------------------------------
+                BeginDrawing();
+                ClearBackground(RAYWHITE);
+
+                DrawText("Pong", 20, 20, 20, DARKGRAY);
+
+                DrawLine(18, 42, screenWidth - 18, 42, BLACK);
+
+                // Player One -- left 
+                DrawRectangle((int)playerOnePosition.X, (int)playerOnePosition.Y, 50, 150,  BLUE);
+
+                // Player Two -- right
+                DrawRectangle((int)playerTwoPosition.X, (int)playerTwoPosition.Y, 50, 150,  RED);
+
+                EndDrawing();
+                //----------------------------------------------------------------------------------
             }
 
-            Raylib.CloseWindow();
+            // De-Initialization
+            //--------------------------------------------------------------------------------------
+            CloseWindow();        // Close window and OpenGL context
+            //--------------------------------------------------------------------------------------
+
+            return 0;
         }
     }
 }
